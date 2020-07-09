27aa115a4bd976ab76380edd89f8a69fb721d494
Tatenda Makandigona <64618097+digitaltk@users.noreply.github.com>
2020-07-10 00:53:15 +0200

    Web Page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Footer = () => {
+  const [bgStyle] = useState({ backgroundColor: "#f5f5f5" });
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-3 text-center">
+      {/* <strong> &copy; 2019 </strong>*/}
+      <i className="fas fa-code"></i> with <i className="fas fa-heart"></i> by{" "}
+      <a
+        className="badge badge-dark"
+        rel="noopener"
+        href="https://github.com/hashirshoaeb"
+        aria-label="My GitHub"
+      >
+        Tatenda Makandigona
+      </a>{" "}
+      using <i className="fab fa-react"></i>
+      <p>
+        <small className="text-muted">
+          {" "}
+          Project code.
+        </small>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
